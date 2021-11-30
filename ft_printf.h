104703c2e5b4a1a5e5378910d71180b0d4871ebd
Oussama Bouizgarne <obouizga@e1r11p9.1337.ma>
2021-11-30 14:18:23 +0100

    Finished version

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: obouizga <obouizga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 14:56:08 by obouizga          #+#    #+#             */
-/*   Updated: 2021/11/30 13:09:02 by obouizga         ###   ########.fr       */
+/*   Updated: 2021/11/30 14:13:59 by obouizga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@
 void	ft_putstr(const char *str, size_t *count);
 void	ft_putchar(char c, size_t *count);
 int		isalpha_f(char c);
-void	ft_putnbr_base16(int nbr, int upper, size_t *count);
 void	ft_putnbr(int nb, size_t *count);
 int		ft_printf(const char *format, ...);
 void	ft_treat(int val, char f, size_t *count);
